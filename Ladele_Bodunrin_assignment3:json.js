61d7b7bcb355f3f2dc02436b2533bab0b7486f8d
Bodunrin Ladele <bladele@me.com>
2012-04-17 12:46:02 -0400

    "Recoded the json.js file; turned the data inside my object into and array, which as the instruction says is better for organizing and retrieving data from your object."

--- Ladele_Bodunrin_assignment3:json.js
+++ Ladele_Bodunrin_assignment3:json.js
@@ -4,30 +4,34 @@
 // How Viewing Entertainment is consumed in my home.
 
 var json = {
- 	"family": {
- 		"001" : {
+ 	"family": [ 
+ 		{
+ 			"uuid" : "001"
  			"name": "Bibi",
  			"age" : 32,
  			"device": "iphone 4",
  			"usage" : [reading, gaming, reference]
- 			},
- 		"002" : {
+ 		},
+ 		{
+ 			"uuid" : "002"
  			"name": "Jenny",
  			"age" : 30,
- 			"device" : "iphone 3GS",
- 			"usage" : []
- 			},
- 		"003" : {
+ 			"device": "iphone 3GS",
+ 			"usage" : [reading, gaming, reference]
+ 		},
+ 		{
+ 			"uuid" : "003"
  			"name": "Audrey",
  			"age" : 10,
- 			"device": "iphone 4",
+ 			"device": "iphone 3GS",
  			"usage" : [reading, gaming, reference]
- 			},
- 		"004" : {
+ 		},
+ 		{
+ 			"uuid" : "004"
  			"name": "Ayo",
  			"age" : 4,
- 			"device": "iphone 4",
+ 			"device": "ipod touch",
  			"usage" : [reading, gaming, reference]
- 			}
  		}
+ 	]
 };
