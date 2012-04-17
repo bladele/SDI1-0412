4a7dae301721cfa23e0e8dd280ce436fff59da87
Bodunrin Ladele <bladele@me.com>
2012-04-17 16:17:40 -0400

    "Trying to fix the issue by organizing my files."

--- js/Ladele_Bodunrin_assignment3.js
+++ js/Ladele_Bodunrin_assignment3.js
@@ -4,11 +4,7 @@
 // Control File
 
 var handleData = function (json) {
-	for (var i = 0; i < json.family.length; i++){
-		var family = json.family[i];
-		console.log("User ID: : " + friend.uuid + ", Name: " + family.name + ", Age: " +
-		famliy.age);
-		};
+	console.log(json);
 };
 
 handleData(myHouse);
