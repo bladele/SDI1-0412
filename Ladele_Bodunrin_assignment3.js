ff0a74da47234c629b68057156358bb5bbd568d1
Bodunrin Ladele <bladele@me.com>
2012-04-16 20:11:55 -0400

    "Committing week 3 assignment, the first iteration of the project."

---
+++ Ladele_Bodunrin_assignment3.js
@@ -0,0 +1,10 @@
+// Family TV Watching Habits
+// Author: Bodunrin Ladele
+// Created for: SDI Online
+// How Viewing Entertainment is consumed in my home.
+
+var dad = { name: "Bibi", age: 32, devices: ["iphone 4", "macbook pro", "apple tv"]}
+var mom = { name: "Jenny", age: 30, devices: ["iphone 3GS", "apple tv"]}
+var daughter = { name: "Audrey", age: 10, devices: ["iphone 3GS", "macbook", "apple tv","wii"]}
+var son = { name: "Ayo", age: 4, devices: ["ipod touch", "apple tv", "wii"]}
+
