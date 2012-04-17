1e13326f43bb2bf812fdf73e0cf8e7e228b2e4cb
Bodunrin Ladele <bladele@me.com>
2012-04-17 16:29:11 -0400

    "I finally got it to read my json data. Sweet!!"

--- js/Ladele_Bodunrin_assignment3_jsondata.js
+++ js/Ladele_Bodunrin_assignment3_jsondata.js
@@ -9,29 +9,25 @@ var myHouse = {
  			"uuid" : "001",
  			"name": "Bibi",
  			"age" : 32,
- 			"device": "iphone 4",
- 			"usage" : [reading, gaming, reference]
+ 			"device": "iphone 4"
  		},
  		{
  			"uuid" : "002",
  			"name": "Jenny",
  			"age" : 30,
- 			"device": "iphone 3GS",
- 			"usage" : [reading, gaming, reference]
+ 			"device": "iphone 3GS"
  		},
  		{
  			"uuid" : "003",
  			"name": "Audrey",
  			"age" : 10,
- 			"device": "iphone 3GS",
- 			"usage" : [reading, gaming, reference]
+ 			"device": "iphone 3GS"
  		},
  		{	
  			"uuid" : "004",
  			"name": "Ayo",
  			"age" : 4,
- 			"device": "ipod touch",
- 			"usage" : [reading, gaming, reference]
+ 			"device": "ipod touch"
  		}
  	]
 };
