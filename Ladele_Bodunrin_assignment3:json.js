fc8fae903dabb3725b0bac8fdc3bed9f5ae1e758
Bodunrin Ladele <bladele@me.com>
2012-04-17 12:28:26 -0400

    "I am learning a little more about json and how to set up my data structures. Putting it into practice."

---
+++ Ladele_Bodunrin_assignment3:json.js
@@ -0,0 +1,33 @@
+// 
+// Author: Bodunrin Ladele
+// Created for: SDI Online
+// How Viewing Entertainment is consumed in my home.
+
+var json = {
+ 	"family": {
+ 		"001" : {
+ 			"name": "Bibi",
+ 			"age" : 32,
+ 			"device": "iphone 4",
+ 			"usage" : [reading, gaming, reference]
+ 			},
+ 		"002" : {
+ 			"name": "Jenny",
+ 			"age" : 30,
+ 			"device" : "iphone 3GS",
+ 			"usage" : []
+ 			},
+ 		"003" : {
+ 			"name": "Audrey",
+ 			"age" : 10,
+ 			"device": "iphone 4",
+ 			"usage" : [reading, gaming, reference]
+ 			},
+ 		"004" : {
+ 			"name": "Ayo",
+ 			"age" : 4,
+ 			"device": "iphone 4",
+ 			"usage" : [reading, gaming, reference]
+ 			}
+ 		}
+};
