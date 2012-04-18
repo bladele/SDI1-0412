79b2bb4e854c06ada386fc98cb34108c617e1393
Bodunrin Ladele <bladele@me.com>
2012-04-18 10:50:03 -0400

    "I have all my attributes in place to create my story."

--- js/Ladele_Bodunrin_assignment3_jsondata.js
+++ js/Ladele_Bodunrin_assignment3_jsondata.js
@@ -9,25 +9,29 @@ var myHouse = {
  			"uuid" : "001",
  			"name": "Bibi",
  			"age" : 32,
- 			"device": "iphone 4"
+ 			"device": "iphone 4",
+ 			"activity" : ["news", "gaming", "reading", "reference", "utility", "entertainment"]
  		},
  		{
  			"uuid" : "002",
  			"name": "Jenny",
  			"age" : 30,
- 			"device": "iphone 3GS"
+ 			"device": "iphone 3GS",
+ 			"activity" : ["news", "reading", "reference", "utility"]
  		},
  		{
  			"uuid" : "003",
  			"name": "Audrey",
  			"age" : 10,
- 			"device": "iphone 3GS"
+ 			"device": "iphone 3GS",
+ 			"activity" : ["news", "gaming", "reference", "utility", "entertainment"]
  		},
  		{	
  			"uuid" : "004",
  			"name": "Ayo",
  			"age" : 4,
- 			"device": "ipod touch"
+ 			"device": "ipod touch",
+ 			"activity" : ["gaming", "reading", "utility", "entertainment"]
  		}
  	]
 };
