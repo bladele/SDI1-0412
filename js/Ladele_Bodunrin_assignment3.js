ad8f2bd71d6f6d3f37544d8d1ef33ccf7b8d00aa
Bodunrin Ladele <bladele@me.com>
2012-04-19 11:49:40 -0400

    "Finally figured out how to call particular attributes from my objects. Going a lot smother now."

--- js/Ladele_Bodunrin_assignment3.js
+++ js/Ladele_Bodunrin_assignment3.js
@@ -3,8 +3,39 @@
 // Created for: SDI Online
 // Control File
 
-var handleData = function (json) {
-	console.log(json);
+console.log("Today we will be looking at the Ladele family and how they use their IOS devices.")
+console.log("Here are the members of the family...")
+
+var handleData = function(json) {
+	for (var i = 0; i < json.family.length; i++) {
+		var family = json.family[i];
+		console.log("Name: " + family.name + ", Designation: " + family.designation + ", Age: " +
+		family.age + ", Device: " + family.device);
+		};
 };
 
-handleData(myHouse);
+handleData(houseHold);
+
+console.log("These are the activities that each member of the family uses their " +
+		"IOS device for. I have also listed the tech adoption rate of each family member.");
+		
+var handleData = function(json) {
+	for (var i = 0; i < json.family.length; i++) {
+		var family = json.family[i];
+		console.log("Name: " + family.name + ", Activity: " + family.activity + ", Adoption Rate: " +
+		family.adoptionRate);
+		};
+};
+
+handleData(houseHold);
+
+var expertise = function(json) {
+	for (var i = 0; i <json.family.length; i++) {
+	var family = json.family[i];
+	console.log("To calculate" + family.activity.length * family.adoptionRate);
+	};
+};
+expertise(houseHold);
+
+
+console.log(houseHold.family[0].name);
--- js/Ladele_Bodunrin_assignment3_jsondata.js
+++ js/Ladele_Bodunrin_assignment3_jsondata.js
@@ -3,35 +3,48 @@
 // Created for: SDI Online
 // Data File
 
-var myHouse = {
+var houseHold = {
  	"family": [ 
  		{
  			"uuid" : "001",
  			"name": "Bibi",
+ 			"designation": "Geeky Dad",
+ 			"sex": "male",
  			"age" : 32,
  			"device": "iphone 4",
- 			"activity" : ["news", "gaming", "reading", "reference", "utility", "entertainment"]
+ 			"activity" : ["news", " gaming", " reading", " reference", " utility", " entertainment", "communication", "productivity"],
+ 			"adoptionRate": 10
  		},
  		{
  			"uuid" : "002",
  			"name": "Jenny",
+ 			"designation": "Loving Mom",
+ 			"sex": "female",
  			"age" : 30,
  			"device": "iphone 3GS",
- 			"activity" : ["news", "reading", "reference", "utility"]
+ 			"activity" : ["news", " reading", " reference", " utility", "communication"],
+ 			"adoptionRate": 5
+
  		},
  		{
  			"uuid" : "003",
  			"name": "Audrey",
+ 			"designation": "Techie tween",
+ 			"sex": "female",
  			"age" : 10,
  			"device": "iphone 3GS",
- 			"activity" : ["news", "gaming", "reference", "utility", "entertainment"]
+ 			"activity" : ["news", " gaming", " reference", " utility", " entertainment", "communication"],
+ 			"adoptionRate": 8
  		},
  		{	
  			"uuid" : "004",
  			"name": "Ayo",
+ 			"designation": "iPod padawan",
+ 			"sex": "male",
  			"age" : 4,
  			"device": "ipod touch",
- 			"activity" : ["gaming", "reading", "utility", "entertainment"]
+ 			"activity" : ["gaming", " reference", " reading", " utility", " entertainment"],
+ 			"adoptionRate": 7
  		}
  	]
 };
