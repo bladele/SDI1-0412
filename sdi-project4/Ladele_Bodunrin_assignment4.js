eb13448d4b9acf51c0ab76272b75b9c9e7e3483c
Bodunrin Ladele <bladele@me.com>
2012-04-24 13:09:22 -0400

    My project 4 files.

---
+++ sdi-project4/Ladele_Bodunrin_assignment4.js
@@ -0,0 +1,8 @@
+// Library: Members Directory 
+// Author: Bodunrin Ladele
+// Created for: SDI Online
+// Control File
+
+
+var addMember: function() {
+};
---
+++ sdi-project4/Ladele_Bodunrin_assignment4_jsondata.js
@@ -0,0 +1,19 @@
+// Members Directory 
+// Author: Bodunrin Ladele
+// Created for: SDI Online
+// Data File
+
+var directory = {
+ 	"members": [ 
+ 		{
+ 			"lastName": "Bibi",
+ 			"firstName": "Ladele",
+ 			"age" : 32,
+ 			"dateOfBirth": 12-24-1979,
+ 			"phoneNumber":704-701-6149,
+ 			"email" : "bladele@me.com,"
+ 			"URL":"",
+ 			"family":[]
+ 		}
+ 	]
+};
