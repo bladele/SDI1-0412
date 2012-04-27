a3ed9f335e586a998118705891ada73f488b6481
Bodunrin Ladele <bladele@me.com>
2012-04-26 21:51:18 -0400

    Still don't have six this is depressing

--- sdi-project4/Utility.js
+++ sdi-project4/Utility.js
@@ -33,13 +33,53 @@ Utility.isEmail = function(checkString)
 	return result;
 };
 
-// Check to see if the given string is a valid URL (begins with http or https)
+// Check to see if the given string is a valid email address
 Utility.isURL = function(checkString)
 {
 	var patt1 = /^http[s]?:/; 
 	var result = patt1.test(checkString);
 	return result;
 }
+
+
+// see if Check is within pct Percent of numberRef
+Utility.fuzzyMatch = function(numberRef, numberCheck, pct)
+{
+	var bigNum = numberRef + (numberRef * (pct/100));
+	var lilNum = numberRef - (numberRef * (pct/100));
+	
+	var result = ( numberCheck <= bigNum || numberCheck >= lilNum);
+	
+	return result;
+}
+
+
+/* Find the smallest value in an array that is greater
+than any given numer*/
+Utility.smallestGreaterThan = function(check, array)
+{
+}
+
+
+/* Given an array of object and name of a key, return the array
+sorted by the value of that key in each of the objects.*/
+Utility.keySort = function(keyName, array)
+{
+}
+
+/* Return the passed in string as an integer.  If the string
+ is NOT a valid integer, return 0*/
+Utility.stringToNum = function(strNum)
+{
+	var theNum = parseInt(strNum);
+	var theResult = 0;
+	if (theNum = Nan)	//I dont know what I am doing wrong here, it seems logical...
+	{
+		theResult = theNum;
+	}
+	return theResult;
+}
+
 
 // Given a decial, expand or truncate it to the given number of places.
 Utility.formatDecimal = function(num, places)
@@ -62,37 +102,7 @@ Utility.dateDiff = function(startDate, endDate)
 	
 }
 
-// see if Check is within pct Percent of numberRef
-Utility.fuzzyMatch = function(numberRef, numberCheck, pct)
-{
-	var bigNum = numberRef + (numberRef * (pct/100));
-	var lilNum = numberRef - (numberRef * (pct/100));
-	
-	var result = ( numberCheck <= bigNum || numberCheck >= lilNum);
-	
-	return result;
-}
 
-Utility.smallestGreaterThan = function(check, array)
-{
-}
-
-utility.keySort = function(keyName, array)
-{
-}
-
-/* Return the passed in string as an integer.  If the string
- is NOT a valid integer, return 0*/
-Utility.stringToNum = function(strNum)
-{
-	var theNum = ParseInt(strNum);
-	var theResult = 0;
-	if (theNum != Nan)
-	{
-		theResult = theNum;
-	}
-	return theResult;
-}
 
 // Find a sum of the number values in an array
 Utility.findSumOfNumbers = function(theArray)
@@ -113,4 +123,6 @@ Utility.findSumOfNumbers = function(theArray)
 
 /* Title-case a string: Split a string into 2 words and 
 change to uppercase first letter of each word.*/
-uti
+Utility.lowerToUpper = function(upperCase)
+{
+}
--- sdi-project4/testUtility.js
+++ sdi-project4/testUtility.js
@@ -3,7 +3,7 @@
 // Created for: SDI Online
 // Test File
 
-
+// Use RegExp to check for valid phone number
  function checkPhone()
 	{
 		var strNum = document.getElementById("txtPhone").value;
@@ -20,6 +20,8 @@
 		alert("The text " + strNum + " " + strResult + " a valid phone number!");
 	}
 	
+	
+// Check to see if the given string is a valid email address
 function checkEmail()
 	{
 		var strMail = document.getElementById("txtEmail").value;
@@ -36,6 +38,8 @@ function checkEmail()
 		alert("The text " + strMail + " " + strResult + " a valid email address!");
 	}
 	
+	
+// Check to see if the given string is a valid URL (begins with http or https)
 function checkUrl()
 	{
 		var strUrl = document.getElementById("txtUrl").value;
@@ -52,7 +56,9 @@ function checkUrl()
 		alert("The text " + strUrl + " " + strResult + " a valid Url!");
 	}
 	
-   
+	
+	
+// see if Check is within pct Percent of numberRef
 function checkFuzzyMatch()
 	{
 		var refNum = document.getElementById("refNum").value;
@@ -72,3 +78,43 @@ function checkFuzzyMatch()
 		alert("The number " + checkNum + " " + strResult + " within " + pct + " percent of " + refNum + "!");
 		
 	}
+	
+	
+/* Return the number within a string as an integer */
+
+function howMuch()
+	{
+		var strSum = document.getElementById("txtNum").value;
+		var givMe = Utility.stringToNum(strSum);
+		var hereUgo;
+		if (givMe >= 0)
+		//I dont know what I am doing wrong here.
+		{
+		alert("Here's $" + hereUgo + ".")
+	    }
+	}
+
+//I dont know what I am doing wrong here.
+
+// find hours or days difference
+function daysbtween()
+	{
+		var numDays = document.getElementById("").value
+	}
+
+
+/* Find the smallest value in an array that is greater
+than any given numer*/
+
+
+
+
+/* Given an array of object and name of a key, return the array
+sorted by the value of that key in each of the objects.*/
+
+
+// Find a sum of the number values in an array
+
+
+/* Title-case a string: Split a string into 2 words and 
+change to uppercase first letter of each word.*/
